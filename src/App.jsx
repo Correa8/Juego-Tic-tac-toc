9f9503c3e2776ca13ee040a67f578073ef1e7619
Daniela Correa <correadaniela783@gmail.com>
2023-04-17 20:07:54 -0500

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ import { checkWinnerFrom, checkEnGame } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+   return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFormStorage = window.localStorage.getItem('turn')
+    return turnFormStorage ?? TURNS.X
+  });
   //null no hay ganador, false hay empate
   const [winner, setWinner] = useState(null);
 
@@ -16,6 +22,10 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+
   };
 
   const updateBoard = (index) => {
@@ -28,6 +38,9 @@ function App() {
     // cambiar turno de x - o
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+  //guardar partida
+  window.localStorage.setItem('board', JSON.stringify(newBoard))
+  window.localStorage.setItem('turn', turn)
 
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
